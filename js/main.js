bac14a0754f4575823abf3f3473b3692ca350823
ZatolokinPavel <Zatolokin.Pavel@gmail.com>
2019-05-11 16:19:11 +0300

    use PhotoSwipe better

--- js/main.js
+++ js/main.js
@@ -12,20 +12,74 @@
     }
 })();
 
-function openPhotoSwipeGallery(allImages, currentImage) {
-    var pswpElement = document.getElementById('photoswipe');
-    console.log(allImages);
-    var items = [
-        {src: 'https://placekitten.com/600/400', w: 600, h: 400},
-        {
-            src: 'https://placekitten.com/1200/900',
-            w: 1200,
-            h: 900
+
+
+var initPhotoSwipeGallery = function(gallerySelector) {
+
+    var _init = function () {
+        var galleryElements = document.querySelectorAll(gallerySelector);
+        for (var i=0; i < galleryElements.length; i++) {
+            galleryElements[i].addEventListener('click', clickOnPhotoSwipeGallery);
         }
-    ];
-    var options = {
-        index: 0 // start at first slide
     };
-    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.init();
-}
+
+    var clickOnPhotoSwipeGallery = function (evt) {
+        evt = evt || window.event;
+        evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+        var evtTarget = evt.target || evt.srcElement;
+        var currItem = closestByTagName(evtTarget, 'A');    // ищем выбранный элемент галереи, вида <a href="" data-size=""><img src=""></a>
+        if (currItem) {
+            var gallery = closestByClassName(currItem, 'pswp__gallery');
+            var images = gallery.getElementsByTagName('IMG');
+            var currImage = currItem.children[0];
+            openPhotoSwipeGallery(images, currImage);
+        }
+    };
+
+    var openPhotoSwipeGallery = function (images, currImage) {
+        var pswpElement = document.getElementById('photoswipe');
+        var items = [], firstSlide = 0;
+        for (var i=0; i < images.length; i++) {
+            if (images[i] === currImage) firstSlide = i;
+            var link = images[i].parentNode;
+            var size = link.getAttribute('data-size').split('x');
+            items.push({
+                src: link.getAttribute('href'),
+                msrc: images[i].getAttribute('src'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10),
+                el: images[i]
+            });
+        }
+        var options = {
+            index: firstSlide,      // start at first slide
+            getThumbBoundsFn: function(index) {
+                var thumbnail = items[index].el,
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect();
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+        };
+        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    var closestByTagName = function (node, tagName) {
+        while (node) {
+            if (node.tagName && node.tagName.toUpperCase() === tagName) return node;
+            else node = node.parentElement;
+        }
+        return null;
+    };
+
+    var closestByClassName = function (node, className) {
+        while (node) {
+            if (node.classList.contains(className)) return node;
+            else node = node.parentElement;
+        }
+        return null;
+    };
+
+    _init();
+};
+initPhotoSwipeGallery('.pswp__gallery');
