009f1f285813272c17b03f0b5e733fbaafb952eb
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2019-05-21 23:53:05 +0300

    script for photoshop

---
+++ utilities/Photoshop/preparingImagesForSite.jsx
@@ -0,0 +1,45 @@
+﻿#target photoshop
+
+var doc = app.activeDocument;
+
+function main() {
+    var originWidth = doc.width.as("px");
+    var needWidth = prompt("Задайте необходимую ширину изображения (px)", originWidth, "Сохранить для Web с учётом DPR");
+    if (!needWidth) return;
+    var fullName = doc.name;
+    var dotIndex = fullName.lastIndexOf('.');
+    var name = (dotIndex !== -1) ? fullName.substr(0, dotIndex) : fullName;
+
+    var dprRatio = [1, 1.5, 2, 2.5, 3, 3.5, 4];
+
+    for (var i=0; i < dprRatio.length; i++) {
+        var width = needWidth * dprRatio[i];
+        if (width > originWidth) continue;
+        var u = (needWidth < 1000) ? '_' : '';
+        var whole = ~~dprRatio[i];
+        var fract = (dprRatio[i] - whole) * 10;
+        var newFile = File(doc.path+'/'+name+ u +needWidth+'_'+whole+'x'+fract+'.jpg');
+        SaveForWebWithResize(newFile, 30, width);
+    }
+}
+main();
+
+function SaveForWebWithResize(newFile, jpegQuality, width, height) {
+    var savedState = doc.activeHistoryState;    // сохраняемся, чтобы откатить ресайз
+    width = width ? UnitValue(width,"px") : null;
+    height = height ? UnitValue(height,"px") : null;
+    doc.resizeImage(width, height, null, ResampleMethod.BICUBIC);
+    SaveForWeb(newFile, jpegQuality);
+    doc.activeHistoryState = savedState;        // откатили изменения
+}
+
+function SaveForWeb(newFile, jpegQuality) {
+    var sfwOptions = new ExportOptionsSaveForWeb();
+    sfwOptions.format = SaveDocumentType.JPEG;   
+    sfwOptions.includeProfile = false;   
+    sfwOptions.interlaced = false;   
+    sfwOptions.optimized = true;   
+    sfwOptions.quality = jpegQuality; //0-100
+    sfwOptions.blur = 0;
+    activeDocument.exportDocument(newFile, ExportType.SAVEFORWEB, sfwOptions);
+}
