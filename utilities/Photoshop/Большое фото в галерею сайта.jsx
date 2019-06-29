87a79f743d7fd0f5abea3033545880c02bc389e6
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2019-06-29 18:36:38 +0300

    add Photoshop scripts for preparing portfolio photos

---
+++ utilities/Photoshop/Большое фото в галерею сайта.jsx
@@ -0,0 +1,42 @@
+﻿#target photoshop
+
+var doc = app.activeDocument;
+
+function main() {
+    var needMaxSize = 1200;
+    var originWidth = doc.width.as("px");
+    var originHeight = doc.height.as("px");
+    var fullName = doc.name;
+    var dotIndex = fullName.lastIndexOf('.');
+    var name = (dotIndex !== -1) ? fullName.substr(0, dotIndex) : fullName;
+    var newFile = File(doc.path+'/'+name+'.jpg');
+
+    if (originWidth >= originHeight) {
+        if (originWidth < needMaxSize) needMaxSize = originWidth;
+        SaveForWebWithResize(newFile, 30, needMaxSize, null);
+    } else {
+        if (originHeight < needMaxSize) needMaxSize = originHeight;
+        SaveForWebWithResize(newFile, 30, null, needMaxSize);
+    }
+}
+main();
+
+function SaveForWebWithResize(newFile, jpegQuality, width, height) {
+    var savedState = doc.activeHistoryState;    // сохраняемся, чтобы откатить ресайз
+    width = width ? UnitValue(width,"px") : null;
+    height = height ? UnitValue(height,"px") : null;
+    doc.resizeImage(width, height, null, ResampleMethod.BICUBIC);
+    SaveForWeb(newFile, jpegQuality);
+    doc.activeHistoryState = savedState;        // откатили изменения
+}
+
+function SaveForWeb(newFile, jpegQuality) {
+    var sfwOptions = new ExportOptionsSaveForWeb();
+    sfwOptions.format = SaveDocumentType.JPEG;   
+    sfwOptions.includeProfile = false;   
+    sfwOptions.interlaced = false;   
+    sfwOptions.optimized = true;   
+    sfwOptions.quality = jpegQuality; //0-100
+    sfwOptions.blur = 0;
+    activeDocument.exportDocument(newFile, ExportType.SAVEFORWEB, sfwOptions);
+}
---
+++ utilities/Photoshop/Маленькие превью в галерею сайта.jsx
@@ -0,0 +1,43 @@
+﻿#target photoshop
+
+var doc = app.activeDocument;
+
+function main() {
+    var originWidth = doc.width.as("px");
+    var needWidth = 345;
+    var fullName = doc.name;
+    var dotIndex = fullName.lastIndexOf('.');
+    var name = (dotIndex !== -1) ? fullName.substr(0, dotIndex) : fullName;
+
+    var dprRatio = [1, 1.5, 2, 2.5, 3, 3.5, 4];
+
+    for (var i=0; i < dprRatio.length; i++) {
+        var width = needWidth * dprRatio[i];
+        if (width > originWidth) continue;
+        var whole = ~~dprRatio[i];
+        var fract = (dprRatio[i] - whole) * 10;
+        var newFile = File(doc.path+'/'+name+'_'+whole+'x'+fract+'.jpg');
+        SaveForWebWithResize(newFile, 30, width);
+    }
+}
+main();
+
+function SaveForWebWithResize(newFile, jpegQuality, width, height) {
+    var savedState = doc.activeHistoryState;    // сохраняемся, чтобы откатить ресайз
+    width = width ? UnitValue(width,"px") : null;
+    height = height ? UnitValue(height,"px") : null;
+    doc.resizeImage(width, height, null, ResampleMethod.BICUBIC);
+    SaveForWeb(newFile, jpegQuality);
+    doc.activeHistoryState = savedState;        // откатили изменения
+}
+
+function SaveForWeb(newFile, jpegQuality) {
+    var sfwOptions = new ExportOptionsSaveForWeb();
+    sfwOptions.format = SaveDocumentType.JPEG;   
+    sfwOptions.includeProfile = false;   
+    sfwOptions.interlaced = false;   
+    sfwOptions.optimized = true;   
+    sfwOptions.quality = jpegQuality; //0-100
+    sfwOptions.blur = 0;
+    activeDocument.exportDocument(newFile, ExportType.SAVEFORWEB, sfwOptions);
+}
