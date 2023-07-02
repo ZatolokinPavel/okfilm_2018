b6d1d44721ee9dc4cfab44e5d0d30cb11a1c7a64
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2023-07-03 00:08:41 +0300

    test shared_files_json

---
+++ js/files.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const DownloadFiles = function () {
+
+    const init = function () {
+        const path = window.location.pathname.replace(/^\/more\/files/, '');
+        fetch('/files/json/'+path, {method: 'GET'})
+            .then(response => response.ok ? response : throw new Error(response.status+' '+response.statusText))
+            .then(response => response.json())
+            .then(json => drawFilesList(json))
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    const drawFilesList = function (json) {
+        console.log('flist', json);
+    };
+
+    init();
+};
+
+new DownloadFiles();
