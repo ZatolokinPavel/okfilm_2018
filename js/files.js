b0955bb06ece03c087c2831a1e47ac27c9d5adb9
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2023-07-03 00:26:11 +0300

    test shared_files_json

--- js/files.js
+++ js/files.js
@@ -5,7 +5,7 @@ const DownloadFiles = function () {
     const init = function () {
         const path = window.location.pathname.replace(/^\/more\/files/, '');
         fetch('/files/json/'+path, {method: 'GET'})
-            .then(response => response.ok ? response : (throw new Error(response.status+' '+response.statusText)))
+            .then(response => response.ok ? response : throw(new Error(response.status+' '+response.statusText)))
             .then(response => response.json())
             .then(json => drawFilesList(json))
             .catch(error => {
